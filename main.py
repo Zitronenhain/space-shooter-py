0f2d2e2adfe79d8f738903c6a122d4560c6e3c28
K. Schalau <k.schalau@maschinenorganismus.de>
2025-01-04 04:26:52 +0100

    update: add asteroids
    Signed-off-by: K. Schalau <k.schalau@maschinenorganismus.de>

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ player_surface = pygame.image.load(join('materials','images','player.png')).conv
 player_rect = player_surface.get_frect( center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 #STARS:
 star_surface = pygame.image.load(join('materials','images','star.png')).convert_alpha()
-star_height,star_width = star_surface.get_height(),star_surface.get_width()
+star_height, star_width = star_surface.get_height(), star_surface.get_width()
 star_positions = [(randint(0, (WINDOW_WIDTH - star_width)), randint(0, (WINDOW_HEIGHT - star_height))) for i in range(20)]
 
+asteroid_surface = pygame.image.load(join('materials','images','asteroid.png')).convert_alpha()
+asteroid_height, asteroid_width = asteroid_surface.get_height(), asteroid_surface.get_width()
+asteroid_positions = [(randint(0, (WINDOW_WIDTH - star_width)), randint(0, (WINDOW_HEIGHT - star_height))) for i in range(8)]
 
-x = 100
-y = 100
-
-while(running):
+while running:
     #EVENT_LOOP:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -31,8 +31,11 @@ while(running):
 
     #DRAWING:
     display_surface.fill('gray11')
-    for position in star_positions:
-        display_surface.blit(star_surface, position)
+    for star_position in star_positions:
+        display_surface.blit(star_surface, star_position)
+    for asteroid_position in asteroid_positions:
+        display_surface.blit(asteroid_surface, asteroid_position)
+
     player_rect.left+=0.1
 
     display_surface.blit(player_surface, player_rect)
